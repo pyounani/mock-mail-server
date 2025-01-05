aeaeb56e03f761f27903615846bf806a76c24fad
pyounani <yndbsk9372@gmail.com>
2025-01-05 23:17:17 +0900

    [Init] 모의 메일 서버 프로젝트 초기 설정

---
+++ MockMailServer/src/main/java/com/pyounani/MockMailServer/MockMailServerApplication.java
@@ -0,0 +1,13 @@
+package com.pyounani.MockMailServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockMailServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockMailServerApplication.class, args);
+	}
+
+}
---
+++ MockMailServer/src/test/java/com/pyounani/MockMailServer/MockMailServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pyounani.MockMailServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockMailServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
