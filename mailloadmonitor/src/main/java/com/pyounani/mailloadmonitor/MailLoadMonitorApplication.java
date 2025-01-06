7fe43d70b9a888aeacc58f1c6f7f8dca8542fb2e
pyounani <yndbsk9372@gmail.com>
2025-01-06 09:45:55 +0900

    [Init] 모의 메일 서버에 부하를 줄 수 있는 프로젝트 생성

---
+++ mailloadmonitor/src/main/java/com/pyounani/mailloadmonitor/MailLoadMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.pyounani.mailloadmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailLoadMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailLoadMonitorApplication.class, args);
+	}
+
+}
---
+++ mailloadmonitor/src/test/java/com/pyounani/mailloadmonitor/MailLoadMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pyounani.mailloadmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailLoadMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
