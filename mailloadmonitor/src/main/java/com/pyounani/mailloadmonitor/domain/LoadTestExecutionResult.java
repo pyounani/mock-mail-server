f8ee7ff50e2c66b659162596f0c2a56b87691b83
pyounani <yndbsk9372@gmail.com>
2025-01-06 10:02:00 +0900

    [Feat] 비동기 부하 테스트를 위한 엔티티 설계

---
+++ mailloadmonitor/src/main/java/com/pyounani/mailloadmonitor/domain/LoadTestExecutionResult.java
@@ -0,0 +1,27 @@
+package com.pyounani.mailloadmonitor.domain;
+
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * 각각의 부하 테스트들에 대한 실행 결과
+ */
+@Entity
+@NoArgsConstructor
+public class LoadTestExecutionResult {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private LoadTestSession loadTestSession; // 실행결과가 속한 부하 테스트 세션
+
+    private Integer loopIdx; // 실행 반복 횟수
+
+    private LocalDateTime requestTime; // 요청이 시작된 시간
+
+    private LocalDateTime finishTime; // 요청이 끝난 시간
+}
---
+++ mailloadmonitor/src/main/java/com/pyounani/mailloadmonitor/domain/LoadTestSession.java
@@ -0,0 +1,29 @@
+package com.pyounani.mailloadmonitor.domain;
+
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * 부하 테스트 세션
+ * 하나의 테스트 세션은 부하 테스트를 실행하기 위한 설정과 메타데이터를 저장
+ */
+@Entity
+@NoArgsConstructor
+public class LoadTestSession {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description; // 테스트 세션에 대한 설명
+
+    private String testParams; // 테스트에 사용된 파라미터
+
+    private LocalDateTime startTime; // 테스트가 시작된 시간
+
+    @OneToMany(mappedBy = "loadTestSession")
+    private List<LoadTestExecutionResult> loadTestExecutionResults; // 테스트 세션에서 발생한 여러 실행 결과들의 목록
+}
