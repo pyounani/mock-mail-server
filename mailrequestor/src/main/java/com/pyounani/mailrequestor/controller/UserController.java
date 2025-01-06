e4e9bc78452cdcf056373bd054f7b8dba011ba15
pyounani <yndbsk9372@gmail.com>
2025-01-06 18:14:08 +0900

    [Fix] 이메일을 파라미터로 전송하도록 수정

--- mailrequestor/src/main/java/com/pyounani/mailrequestor/controller/UserController.java
+++ mailrequestor/src/main/java/com/pyounani/mailrequestor/controller/UserController.java
@@ -1,13 +1,12 @@
 package com.pyounani.mailrequestor.controller;
 
-import com.pyounani.mailrequestor.dto.EmailDTO;
 import com.pyounani.mailrequestor.response.code.ResponseCode;
 import com.pyounani.mailrequestor.response.dto.ResponseDTO;
 import com.pyounani.mailrequestor.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,9 +19,9 @@ public class UserController {
      * 이메일 인증 코드 요청하기
      */
     @PostMapping("/emails/verification-requests")
-    public ResponseEntity<ResponseDTO> sendEmailVerification(@RequestBody EmailDTO emailDTO) {
+    public ResponseEntity<ResponseDTO> sendEmailVerification(@RequestParam String email) {
 
-        userService.sendCodeToEmail(emailDTO.getEmail());
+        userService.sendCodeToEmail(email);
         return ResponseEntity
                 .status(ResponseCode.SUCCESS_VERIFICATION_REQUEST.getStatus().value())
                 .body(new ResponseDTO<>(ResponseCode.SUCCESS_VERIFICATION_REQUEST, null));
